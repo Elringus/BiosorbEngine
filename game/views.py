0acdcf34b06602f89c942d2e354ba243ba70db2d
Elringus <elringus@yandex.ru>
2013-02-05 14:57:14 +0400

    test

--- game/views.py
+++ game/views.py
@@ -23,7 +23,7 @@ def add_score(request):
     userID = int(request.POST.get('userID'))
     user = User.objects.get(pk=userID)
     playerProfile = user.get_profile()
-    playerProfile.score += 1
+    playerProfile.score += 2
     playerProfile.save()
 
 
